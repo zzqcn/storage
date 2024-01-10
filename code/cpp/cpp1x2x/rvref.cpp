7b0fdeab8349e8596dc066579887a4ed8ac613dd
zzqcn <psyc209@163.com>
2024-01-10 14:42:24 +0800

    添加现代C++特性代码

--- code/cpp/cpp1x2x/rvref.cpp
+++ code/cpp/cpp1x2x/rvref.cpp
@@ -32,12 +32,63 @@ lvref make_lvref() {
   return lvref();
 }
 
+class rvref {
+public:
+  rvref() { std::cout << "rvref ctor" << std::endl; }
+  rvref(const rvref &) { std::cout << "rvref copy ctor" << std::endl; }
+  ~rvref() { std::cout << "rvref dtor" << std::endl; }
+  void show() { std::cout << "show rvref" << std::endl; }
+};
+
+rvref make_rvref() {
+  rvref v;
+  return v;
+}
+
+class mempool {
+public:
+  static const int POOL_SIZE = 4096;
+  mempool() : pool(new char[POOL_SIZE]){};
+  ~mempool() {
+    if (pool != nullptr)
+      delete[] pool;
+  }
+  mempool(const mempool &other) : pool(new char[POOL_SIZE]) {
+    std::cout << "copy big mempool" << std::endl;
+    memcpy(pool, other.pool, POOL_SIZE);
+  }
+
+private:
+  char *pool;
+};
+
+mempool get_pool(const mempool &pool) {
+  return pool;
+}
+
+mempool make_pool() {
+  mempool pool;
+  return get_pool(pool);
+}
+
 int unit_rvref::run() {
+  // 常量左值引用
   lvref a;
   lvref b(a);
+  // 如果把lvref的拷贝构造函数和赋值运算符函数参数的const删除, 以下两行将编译失败,
+  // 因为make_lvref()产生的是右值, 它无法绑定上述常量左值引用
   lvref c(make_lvref());
   c = make_lvref();
   std::cout << "c: " << type_name<decltype(c)>() << std::endl;
+
+  std::cout << "normal: " << std::endl;
+  rvref d = make_rvref();
+  d.show();
+  std::cout << "rv ref: " << std::endl;
+  rvref &&e = make_rvref();
+  e.show();
+
+  mempool pool = make_pool();
 
   return 0;
 }
